42a2ae482eb7b0ef2e14f94bacc09e9b95016900
fist_it <fist_it@protonmail.com>
2024-12-07 19:15:30 +0100

    Initial commit

---
+++ main.hs
@@ -0,0 +1,20 @@
+-- zadanie 38:
+-- Liczby B-gładkie to takie, których dzielniki pierwsze są mniejsze lub równe B. Dla danego B i n ile jest liczb
+-- B-gładkich nie przekraczających n? Przykładowo dla B = 5 i n = 30 początkowe liczby 5-gładkie inaczej
+-- zwane liczbami Hamming’a to: 1, 2, 3, 4, 5, 6, 8, 9, 10, 12, 15, 16, 18, 20, 24, 25, 27, 30… więc odpowiedź
+-- wynosi: 18.
+
+-- zadanie 26:
+-- Palindrom 595 możemy zapisać jako sumę kwadratów kolejnych liczb naturalnych: 62 + 72 + 82 +
+-- 92 + 102 + 112 + 122. Dla danego n wydrukować wszystkie palindromy mniejsze od n, które możemy
+-- zapisać jako sumę kwadratów kolejnych liczb naturalnych.
+
+-- zadanie 9:
+-- dla danej liczby naturalnej n podaj dla jakiej liczby naturalnej m <= n
+-- zaczyna sie najdluzszy ciag Collatza
+--
+-- ciag collatza:
+-- n%2 == 0 -> n/2
+-- n%2 == 1 -> 3*n+1
+
+main = putStrLn "Hello, World!"
